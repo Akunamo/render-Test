00c5774959e40ed864d8d907baa9e0ab9a135db3
Aviral <96827211+Akunamo@users.noreply.github.com>
2023-05-21 15:39:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.post('/api/v1/tours', (req, res) => {
 
 
 const PORT = process.env.PORT || 4200;
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Listening on port ${PORT}`);
 })
